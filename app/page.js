eb79fcb9b5cbea41b7d1dcdf815a6d622db47d6f
muslichwr <muslichwahyuromadhon@gmail.com>
2025-12-24 23:48:27 +0700

    feat: Implement a new React homepage with a dynamic like button component.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import LikeButton from './like-button';
 
 function Header({ title }) {
     return <h1>{title ? title : 'Default title'}</h1>;
@@ -6,12 +7,6 @@ function Header({ title }) {
 
 export default function HomePage() {
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-
-    const [likes, setLikes] = useState(0);
-
-    function handleClick() {
-        setLikes(likes + 1);
-    }
 
     return (
         <div>
@@ -21,8 +16,7 @@ export default function HomePage() {
                     <li key={name}>{name}</li>
                 ))}
             </ul>
-
-            <button onClick={handleClick}>Like ({likes})</button>
+            <LikeButton />
         </div>
     );
 }
